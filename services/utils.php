e45d8e8311753bb0a8f7d46402ef4430c7a63a5e
Giovanni Manghi <giovanni.manghi@faunalia.pt>
2016-03-30 09:19:57 +0100

    more form fixes, again

--- services/utils.php
+++ services/utils.php
@@ -60,12 +60,14 @@
 	
 	function sendEmail($to, $msg){
 		$subject = 'Inscrição no 3º Encontro de Utilizadores QGIS Portugal';
-		$headers = 'MIME-Version: 1.0' . "\r\n";
-		$headers .= 'Content-type: text/plain; charset=iso-8859-1' . "\r\n";
-		$headers .= 'From: Grupo de Utilizadores QGIS Portugal <qgis@qgis.pt>' . "\r\n";
-		$headers .= 'Bcc: qgis@qgis.pt' . "\r\n";
+		$headers = 'From: Grupo de Utilizadores QGIS Portugal <qgis@qgis.pt>' . "\r\n";
 		$headers .= 'Reply-To: Grupo de Utilizadores QGIS Portugal <qgis@qgis.pt>' . "\r\n";
-		$headers .= 'X-Mailer: PHP/'.phpversion() . "\r\n";
+		$headers .= 'Return-Path: Grupo de Utilizadores QGIS Portugal <qgis@qgis.pt>' . "\r\n";		
+		$headers .= 'Bcc: qgis@qgis.pt' . "\r\n";
+		$headers .= 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/plain; charset=iso-8859-1' . "\r\n";
+		$headers .= 'X-Priority: 3\r\n';
+		$headers .= 'X-Mailer: PHP'.phpversion() . "\r\n";
 		if (!mail($to, $subject, $msg, $headers)){
 			return false;
 		}
